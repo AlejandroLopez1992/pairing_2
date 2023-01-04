532ce1c346b9a943ff2e22127c672f2828204de7
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-01-04 15:58:40 -0700

    Add elsif for Kyle

--- friends.rb
+++ friends.rb
@@ -1,7 +1,13 @@
-best_friend = "Scott"
+best_friend = "Scot"
+good_friend = "Kye"
 
 if best_friend == "Scott"
     puts "Hey Scott!"
+elsif good_friend == "Kyle"
+    puts "How you doing?"
 else 
     puts "Who are you?"
 end
+
+
+
